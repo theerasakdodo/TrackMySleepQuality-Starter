93bf2c86288e5620999fb389a33727c1634e196b
Theerasak <theerasaksolo@gmail.com>
2020-08-11 02:29:03 +0700

    06.1 Task:6 Step 2: Test the SleepDatabase

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -29,7 +29,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
-
+//
 /**
  * This is not meant to be a full set of tests. For simplicity, most of your samples do not
  * include tests. However, when building the Room, it is helpful to make sure it works before
