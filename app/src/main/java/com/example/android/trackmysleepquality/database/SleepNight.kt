880cbe0ceafe5b58080c62573293540baa3de7e1
Theerasak <theerasaksolo@gmail.com>
2020-08-11 02:09:08 +0700

    06.1 Task:5 Create the DAO

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -23,7 +23,6 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight(
         @PrimaryKey(autoGenerate = true)
-
         var nightId: Long = 0L,
 
         @ColumnInfo(name = "start_time_milli")
@@ -34,4 +33,5 @@ data class SleepNight(
 
         @ColumnInfo(name = "quality_rating")
         var sleepQuality: Int = -1
+
 )
